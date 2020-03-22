db450397162b6a4e47ffaac842d8cb535962e61c
Craig Booker <craig@arcadiasoftworks.com>
2020-03-21 21:23:59 -0500

    Finished adding the tech selection options component.

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,11 @@ import {
 	SET_LOADING,
 	LOGS_ERROR,
 	ADD_LOG,
-	DELETE_LOG
+	DELETE_LOG,
+	UPDATE_LOG,
+	SEARCH_LOGS,
+	SET_CURRENT,
+	CLEAR_CURRENT
 } from './types';
 
 // export const getLogs = () => {
@@ -35,7 +39,7 @@ export const getLogs = () => async dispatch => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data
+			payload: err.response.statusText
 		});
 	}
 };
@@ -61,7 +65,7 @@ export const addLog = log => async dispatch => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data
+			payload: err.response.statusText
 		});
 	}
 };
@@ -82,9 +86,71 @@ export const deleteLog = id => async dispatch => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data
+			payload: err.response.statusText
 		});
 	}
+};
+
+// Update log on server
+export const updateLog = log => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs${log.id}`, {
+			method: 'PUT',
+			body: JSON.stringify(log),
+			headers: {
+				'Context-Type': 'application/json'
+			}
+		});
+
+		const data = await res.json();
+
+		dispatch({
+			type: UPDATE_LOG,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.statusText
+		});
+	}
+};
+
+// Search server logs
+export const searchLogs = text => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs?q=${text}`);
+		const data = await res.json();
+
+		dispatch({
+			type: SEARCH_LOGS,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.statusText
+		});
+	}
+};
+
+// Set current log
+export const setCurrent = log => {
+	return {
+		type: SET_CURRENT,
+		payload: log
+	};
+};
+
+// Clear current log
+export const clearCurrent = () => {
+	return {
+		type: CLEAR_CURRENT
+	};
 };
 
 // Set loading to true
